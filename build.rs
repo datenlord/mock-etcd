ec71266f26a29ce6ceed0095f901a32b34b2baf5
Zheng Pan <zhengan@google.com>
2021-07-09 14:12:36 -0700

    Add lock and lease functions.

--- build.rs
+++ build.rs
@@ -3,10 +3,15 @@ fn main() {
     // run the following command to install:
     // `sudo apt install cmake g++ libprotobuf-dev protobuf-compiler`
     protoc_grpcio::compile_grpc_protos(
-        &["proto/auth.proto", "proto/kv.proto", "proto/etcd.proto"], // inputs
-        &["proto"],                                                  // includes
-        "src",                                                       // output
-        None,                                                        // customizations
+        &[
+            "proto/auth.proto",
+            "proto/kv.proto",
+            "proto/etcd.proto",
+            "proto/lock.proto",
+        ], // inputs
+        &["proto"], // includes
+        "src",      // output
+        None,       // customizations
     )
     .unwrap_or_else(|_| panic!("Failed to compile gRPC definitions!"));
 }
